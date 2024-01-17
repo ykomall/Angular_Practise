9d00c2ba47d078345a31e784b10bd2e40b1fda60
Komal Yadav <komalyadav@192.168.1.3>
2024-01-18 02:07:16 +0530

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,44 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Angular Practise Code';
+
+  count=0;
+  counter(type:string){
+    type === 'add' ? this.count++ : this.count--;
+  }
+
+name='komal';
+  d=false;
+  t=true;
+  condition="yes";
+  color="r";
+  sw_case="b";
+
+  user=['abc','xyz','pqr'];
+  user_detail=[
+    {name : 'abc', id:'123',dept:'SE'},
+    {name : 'xyz', id:'124',dept:'ASE'},
+    {name : 'pqr', id:'125',dept:'ISE'},
+  ]
+
+  uDetail=[
+    {name:'abc' ,social:['ig','fb']},
+    {name:'abc' ,social:['wa','fb']},
+    {name:'abc' ,social:['ig','lk']}, 
+  ]
+
+  clr="red";
+  bgclr="green";
+
+  updateColor(){
+    this.clr="blue";
+    this.bgclr="red";
+  }
+
+}
